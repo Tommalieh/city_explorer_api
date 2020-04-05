3cd9db19f05d9602e2751972b011e769b21f7838
Tommalieh <muhammedtommalieh@gmail.com>
2020-04-05 18:17:50 +0300

    Added location and weather features

--- server.js
+++ server.js
@@ -12,7 +12,54 @@ const cors = require('cors');
 //Application Setup
 const PORT = process.env.PORT || 4000;
 const app = express();
+app.use(cors());
 
+app.get('/', (request, response) => {
+    response.status(200).send('Home Page!');
+  });
 
+app.get('/location', (request, response) => {
+    const geoData = require('./data/geo.json');
+    const requestCity = request.query.city;
+    const locationEnteries = new Location (requestCity, geoData);
+    response.status(200).json(locationEnteries);
+})
+
+app.get('/weather', (request, response) => {
+    const weatherData = require('./data/darksky.json');
+    let weatherDataArr = [];
+    for (let i = 0; i < weatherData.data.length; i++){
+    const weatherEnteries = new LocationWeather (weatherData.data[i]);
+    weatherDataArr.push(weatherEnteries);
+
+}
+    response.status(200).json(weatherDataArr);
+
+})
+
+app.use('*', notFoundHandler);
+
+//Constructor to change to the right format of data
+function Location (requestCity, geoData){
+    this.search_query = requestCity;
+    this.formatted_query = geoData[0].display_name;
+    this.latitude = geoData[0].lat;
+    this.longitude = geoData[0].lon;
+}
+
+function LocationWeather (weatherData){
+    let dateString = new Date(weatherData.valid_date);
+    const options = { weekday: 'short', year: 'numeric', month: 'long', day: '2-digit' };
+    let formattedTime = dateString.toLocaleDateString('en-US', options).split(',').join(' ');
+    // console.log(formattedTime);
+
+    this.forcast = weatherData.weather.description;
+    this.time = formattedTime;
+}
+
+//Helper Functions
+function notFoundHandler(request, response) {
+    response.status(404).send('NOT FOUND!!');
+  }
 
 app.listen(PORT, () => console.log(`We're Live on port ${PORT} BB`));
